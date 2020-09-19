30611fd0a50e7aa18fbe473e1abca695e26411b2
bazukax <kristijonas9871@gmail.com>
2020-09-19 21:40:21 +0300

    Added player dialog,fade

---
+++ PVUnity/Assets/Scripts/DialogBoxController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogBoxController : MonoBehaviour
+{
+    private bool mFaded = false;
+    public float duration = 0.4f;
+
+
+    public void Fade()
+    {
+        var canvasGroup = GetComponent<CanvasGroup>();
+        StartCoroutine(DoFade(canvasGroup, canvasGroup.alpha, mFaded ? 1 : 0));
+
+        mFaded = !mFaded;
+    }
+    public IEnumerator DoFade(CanvasGroup canvasGroup,float start,float end)
+    {
+        float counter = 0f;
+        while(counter < duration)
+        {
+            counter += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(start, end, counter / duration);
+            yield return null;
+        }
+    }
+   
+}
---
+++ PVUnity/Assets/SendMessage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SendMessage : MonoBehaviour
+{
+    public string[] dialogMessage;
+    public Text messageBox;
+    public DialogBoxController dc;
+
+     bool wait = true;
+     float pause = 0;
+     float pauseTime = 3;
+    void Awake()
+    {
+        
+    }
+    //just for testing=
+    void Update()
+    {
+        if (wait == true && pause < pauseTime)
+        {
+            pause += 1 * Time.deltaTime;
+            sendMessage(0);
+        }
+        else
+        {
+            dc.Fade();
+            wait = true;
+            pause = 0;
+        }
+        
+    }
+    //
+    public void sendMessage(int id)
+    {
+        messageBox.text = dialogMessage[id];
+    }
+
+}
