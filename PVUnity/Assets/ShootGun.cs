ed5afb87a66d968560e4f2cfada1155522ee896d
bazukax <kristijonas9871@gmail.com>
2020-09-20 15:55:56 +0300

    Updated weapon

--- PVUnity/Assets/ShootGun.cs
+++ PVUnity/Assets/ShootGun.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class ShootGun : MonoBehaviour
 {
-    //...setting shoot direction
     Vector3 shootDirection;
     public Rigidbody2D projectile;
     public float projectileSpd;
- //...instantiating the rocket
- 
-  
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 mouseScreen = Input.mousePosition;
         Vector3 mouse = Camera.main.ScreenToWorldPoint(mouseScreen);
+       
         transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg );
 
         shootDirection = Input.mousePosition;
@@ -30,8 +22,8 @@ public class ShootGun : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            Rigidbody2D bulletInstance = Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
-            bulletInstance.velocity = new Vector2(shootDirection.x * projectileSpd, shootDirection.y * projectileSpd); //reik padaryt kad direction neitakotu projectile speed xD
+            Rigidbody2D bulletInstance = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody2D;
+            bulletInstance.velocity = new Vector2(shootDirection.x , shootDirection.y).normalized *projectileSpd; 
         }
     }
 }
