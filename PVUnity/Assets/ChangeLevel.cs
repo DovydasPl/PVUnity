641f1534097a744410f3595dd97e9cc13f638eda
bazukax <kristijonas9871@gmail.com>
2020-10-20 14:59:35 +0300

    new level

---
+++ PVUnity/Assets/ChangeLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public int levelID = 0;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(levelID);
+        }
+    }
+}
