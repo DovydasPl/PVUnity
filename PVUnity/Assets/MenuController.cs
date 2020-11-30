3c61c4abe3e5c188328bfba131bd6a6f78a48793
bazukax <kristijonas9871@gmail.com>
2020-11-30 14:12:29 +0200

    main menu added

---
+++ PVUnity/Assets/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+ public void onPlayClick()
+    {
+        Application.LoadLevel(1);
+    }
+    public void onContinueClick()
+    {
+        int level = PlayerPrefs.GetInt("lastLevel",0);
+        Application.LoadLevel(level);
+    }
+    public void onOptionsClick()
+    {
+        
+    }
+    public void onExitClick()
+    {
+        Application.Quit();
+    }
+}
